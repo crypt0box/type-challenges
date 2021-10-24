73939b07dc8a2b8b6f604e5fff94e87bcd9b34f0
crypt0box <pray.happy76@gmail.com>
2021-10-24 20:16:08 +0900

    add: initial commit

--- uhyo-excercise.ts
+++ uhyo-excercise.ts
@@ -1,3 +1,15 @@
+1-4
+function sumOfPos(arr: number[]) {
+  return arr.filter(num => num >= 0).reduce((acc, num) => acc + num, 0);
+}
+
+// 使用例
+const sum: number = sumOfPos([1, 3, -2, 0]);
+
+// エラー例
+sumOfPos(123, 456);
+sumOfPos([123, "foobar"]);
+
 // 1-3
 // type IsPositiveFunc = (num: number) => boolean
 
