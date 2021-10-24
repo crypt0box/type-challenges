2e12a0b00584590fb0846d781cf57a593733885e
crypt0box <pray.happy76@gmail.com>
2021-10-24 20:11:46 +0900

    add: initial commit

---
+++ uhyo-excercise.ts
@@ -0,0 +1,41 @@
+
+
+// 1-2
+// function showUserInfo(user: User) {
+//   // 省略
+// }
+
+// type User = {
+//   name: string;
+//   age: number;
+//   private: boolean;
+// }
+
+// // 使用例
+// showUserInfo({
+//   name: 'John Smith',
+//   age: 16,
+//   private: false,
+// });
+
+// // エラー例
+// showUserInfo({
+//   name: 'Mary Sue',
+//   private: false,
+// });
+// const usr: User = {
+//   name: 'Gombe Nanashino',
+//   age: 100,
+// };
+
+// 1-1
+// function isPositive(num: number): boolean {
+//   return num >= 0;
+// }
+
+// // 使用例
+// isPositive(3);
+
+// // エラー例
+// isPositive('123');
+// const numVar: number = isPositive(-5);
