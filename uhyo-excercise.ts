0710a2fa1210520d607e3d41e984ee71a7028fdc
crypt0box <pray.happy76@gmail.com>
2021-10-24 20:24:37 +0900

    add: initial commit

--- uhyo-excercise.ts
+++ uhyo-excercise.ts
@@ -1,3 +1,25 @@
+// 2-2
+// type Speed = 'slow' | 'medium' | 'fast';
+
+// function getSpeed(speed: Speed): number {
+//   switch (speed) {
+//     case "slow":
+//       return 10;
+//     case "medium":
+//       return 50;
+//     case "fast":
+//       return 200;
+//   }
+// }
+
+// // 使用例
+// const slowSpeed = getSpeed("slow");
+// const mediumSpeed = getSpeed("medium");
+// const fastSpeed = getSpeed("fast");
+
+// // エラー例
+// getSpeed("veryfast");
+
 // 2-1 回答例
 // function myFilter<T>(arr: T[], predicate: (elm: T) => boolean): T[] | T {
 //   const result = [];
